dadf68271ae7872becffde9ffd82a325f90b3ce8
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-05-09 14:42:01 +0900

    first

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" v-model='test' placeholder=".shop주소 도메인명을 쓰시오">
+    <button v-on:click="move">뭅뭅</button>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const test = ref('');
+
+  function move() {
+    location.href='https://' + test.value + '.shop/health';
+  }
+</script>
+
+<style scoped>
+
+</style>
